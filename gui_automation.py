25c354c74443f88438af36e8990dc81038d1eecf
Jinen Gandhi <jinen.gandhi@intel.com>
2025-03-04 23:45:55 +0530

    Script modifications for different windows client

--- gui_automation.py
+++ gui_automation.py
@@ -1,6 +1,6 @@
-import subprocess
-import json
 import time
+import os
+import shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -10,51 +10,61 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.download_manager import WDMDownloadManager
 from webdriver_manager.core.http import HttpClient
 from selenium.webdriver.common.action_chains import ActionChains
-import pytesseract
 from requests import Response
 import requests
 import easyocr
 from selenium.webdriver.common.keys import Keys
+import urllib3
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+urllib3.disable_warnings()
+
+if os.path.exists("workspace"):
+    shutil.rmtree("workspace")
+
+os.mkdir("workspace")
 
 # Define the API endpoint
-url = "https://onecloudapi.intel.com/9d8e7555282bc3fc8c19ca90660e37a9/kvmlink/10273"
-
-# Construct the curl command
-curl_command = [
-    "curl",
-    "-X", "GET",
-    url,
-    "-H", f"accept: application/json",
-]
+url = "https://onecloudapi.intel.com/9d8e7555282bc3fc8c19ca90660e37a9/kvmlink/10288"
 
 class CustomHttpClient(HttpClient):
     def get(self, url, params=None) -> Response:
         proxies={'http': 'http://proxy-iind.intel.com:911',
          'https': 'http://proxy-iind.intel.com:912',
          }
-        return requests.get(url, params,proxies=proxies,verify=False)
+        return requests.get(url, params,verify=False)
 
 http_client = CustomHttpClient()
 download_manager = WDMDownloadManager(http_client)
 try:
-    # Run the curl command
-    result = subprocess.run(curl_command, capture_output=True, text=True, check=True)
+    http = urllib3.PoolManager(
+    cert_reqs='CERT_NONE',
+    assert_hostname=False
+    )
 
-    # Parse the JSON response
-    response = json.loads(result.stdout)
+    # Sending a GET request and getting back response as HTTPResponse object.
+    response = http.request('GET', url)
+    print("Response Status:", response.status)
 
-    kvm_link = response["kvmlink"]
+    # Check if the request was successful
+    if response.status == 200:
+        # Parse the JSON response
+        data = response.json()
+        print("Response Data:", data)
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+    kvm_link = data["kvmlink"]
 
     print(f"KVM Link: {kvm_link}")
 
     # Set up Selenium WebDriver
     proxy = "http://proxy-iind.intel.com:912"
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument('--ignore-ssl-errors=yes')
     chrome_options.add_argument('--ignore-certificate-errors')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(download_manager=download_manager).install()),options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     # Open the KVM link in the browser
     driver.get(kvm_link)
@@ -76,16 +86,16 @@ try:
 
     driver.maximize_window()
 
-    driver.execute_script("document.body.style.zoom='50%'")
-
-    screenshot_path = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\sdp.png"
+    driver.execute_script("document.body.style.zoom='70%'")
+    time.sleep(5)
+    screenshot_path = r"workspace\sdp.png"
     driver.save_screenshot(screenshot_path)
 
-    reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+    reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
     result = reader.readtext(screenshot_path, detail = 0, paragraph=True)
     print(result)
 
-    matching = [s for s in result if "login" in s]
+    matching = [s for s in result if "login" in s or "Login" in s]
     if len(matching) > 0:
         print(" ####### Match Found: You are logged into SDP system ########")
     else:
@@ -101,10 +111,10 @@ try:
     start_time = time.time()
     timeout = 120
     while boot_main_screen == False or time.time() - start_time < timeout:
-        boot_main_page = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\boot_main_screen.png"
+        boot_main_page = r"workspace\boot_main_screen.png"
         driver.save_screenshot(boot_main_page)
 
-        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
         result = reader.readtext(boot_main_page, detail = 0, paragraph=True)
         print(result)
 
@@ -120,14 +130,14 @@ try:
     if boot_main_screen == False:
         # Raise an exception if the condition is not met within the timeout
         raise TimeoutError("The system didn't boot up within the specified timeout")
-    
+
     boot_second_screen = False
     already_in_bios_page = False
     while boot_second_screen == False:
-        boot_second_page = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\boot_second_screen.png"
+        boot_second_page = r"workspace\boot_second_screen.png"
         driver.save_screenshot(boot_second_page)
 
-        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
         result = reader.readtext(boot_second_page, detail = 0, paragraph=True)
         print(result)
 
@@ -148,11 +158,11 @@ try:
     
     time.sleep(10)
     if already_in_bios_page == False:
-    
-        bios_main_page = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\bios_main_page.png"
+
+        bios_main_page = r"workspace\bios_main_page.png"
         driver.save_screenshot(bios_main_page)
 
-        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
         result = reader.readtext(bios_main_page, detail = 0, paragraph=True)
         print(result)
 
@@ -168,11 +178,11 @@ try:
             time.sleep(3)
     else:
         raise ValueError("You are not in BIOS Main page, exiting out.")
-    
-    bios_socket_configuration_page = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\bios_socket_configuration_page.png"
+
+    bios_socket_configuration_page = r"workspace\bios_socket_configuration_page.png"
     driver.save_screenshot(bios_socket_configuration_page)
 
-    reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+    reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
     result = reader.readtext(bios_socket_configuration_page, detail = 0, paragraph=True)
     print(result)
 
@@ -190,10 +200,10 @@ try:
     
     time.sleep(5)
 
-    processor_configuration_page = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\processor_configuration_page.png"
+    processor_configuration_page = r"workspace\processor_configuration_page.png"
     driver.save_screenshot(processor_configuration_page)
 
-    reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+    reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
     result = reader.readtext(processor_configuration_page, detail = 0, paragraph=True)
     print(result)
 
@@ -205,14 +215,14 @@ try:
             actions.send_keys(Keys.ARROW_UP)
             actions.perform()
             time.sleep(3)
-        
-        sgx_configuration_page = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\sgx_configuration_page.png"
+
+        sgx_configuration_page = r"workspace\sgx_configuration_page.png"
         driver.save_screenshot(sgx_configuration_page)
 
-        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+        reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
         result = reader.readtext(sgx_configuration_page, detail = 0, paragraph=True)
         print(result)
-        
+
         matching = [s for s in result if "SGX" in s]
         if len(matching) > 0:
             print(" ####### Match Found: You are in SGX Configuration page ########")
@@ -227,21 +237,24 @@ try:
             time.sleep(3)
             actions.send_keys('Y')
             time.sleep(10)
+            
             for i in range(12):
                 actions.send_keys(Keys.ARROW_DOWN)
                 actions.perform()
                 time.sleep(5)
+
             actions.send_keys(Keys.ENTER)
             actions.perform()
             time.sleep(5)
+
             actions.send_keys(Keys.ARROW_DOWN)
             actions.perform()
             time.sleep(5)
 
-            final_configuration_page = r"C:\Users\jinengan\OneDrive - Intel Corporation\Documents\TDX\gui_automation\final_configuration_page.png"
+            final_configuration_page = r"workspace\final_configuration_page.png"
             driver.save_screenshot(final_configuration_page)
 
-            reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\jinengan\\.EasyOCR\\model", download_enabled=False)
+            reader = easyocr.Reader(['en'], gpu=False, model_storage_directory="C:\\Users\\intel\\jinen\\bios_automation\\easyocr_models", download_enabled=False)
             result = reader.readtext(final_configuration_page, detail = 0, paragraph=True)
             print(result)
 
@@ -258,6 +271,8 @@ try:
 
     time.sleep(10)
 
+    print(" ####### SGX Factory Reset Done ########")
+
     # Close the browser
     driver.quit()
 
@@ -265,7 +280,9 @@ except ValueError as e:
     print("Caught an exception", e)
     # Close the browser
     driver.quit()
+    assert False, f"Exiting out"
 except TimeoutError as e:
     print(f"Caught a timeout exception: {e}")
     # Close the browser
     driver.quit()
+    assert False, f"Exiting out"
